3beccef735c38efa2c23a6a4decad16151104f2c
jor-eldevera <jorell.devera@gmail.com>
2023-05-18 20:09:10 -0700

    Test preOrder and postOrder

--- script.js
+++ script.js
@@ -14,12 +14,30 @@ tree.prettyPrintSelf();
 
 console.log(tree.find(4));
 
+tree.prettyPrintSelf();
+console.log("Level order:");
 console.log(tree.levelOrder());
 tree.levelOrder(function (node) {
     console.log(node.value);
 });
 
+tree.prettyPrintSelf();
+console.log("inOrder:");
 console.log(tree.inOrder());
 tree.inOrder(function (node) {
     console.log(node.value);
-})
+});
+
+tree.prettyPrintSelf();
+console.log("preOrder:");
+console.log(tree.preOrder());
+tree.preOrder(function (node) {
+    console.log(node.value);
+});
+
+tree.prettyPrintSelf();
+console.log("postOrder:");
+console.log(tree.postOrder());
+tree.postOrder(function (node) {
+    console.log(node.value);
+});
