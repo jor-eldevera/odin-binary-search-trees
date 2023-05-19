c73b42bdb3ddbd29d5317a89a9e6913317796547
jor-eldevera <jorell.devera@gmail.com>
2023-05-18 18:15:54 -0700

    Test insert

---
+++ script.js
@@ -0,0 +1,10 @@
+import Tree from "./Tree.js";
+
+let tree = new Tree();
+let nums = [1, 3, 2, 5, 4, 5, 3];
+tree.buildTree(nums);
+tree.insert(8);
+tree.insert(7);
+tree.insert(6);
+tree.insert(9);
+tree.prettyPrintSelf();
