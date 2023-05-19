f2eb792e1032c592ed16bc9dd812e16cde514161
jor-eldevera <jorell.devera@gmail.com>
2023-05-18 18:01:38 -0700

    Write buildTree (which uses sortedArrayToBST), add prettyPrint

--- Tree.js
+++ Tree.js
@@ -8,4 +8,53 @@ export default class Tree {
             this.root = new Node(value);
         }
     }
+
+    buildTree(array) {
+        // Sort array
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        
+        // Remove duplicates
+        let noDuplicates = [...new Set(array)];
+
+        // call sortedArrayToBST
+        this.root = this.sortedArrayToBST(noDuplicates, 0, noDuplicates.length - 1)
+        this.prettyPrint(this.sortedArrayToBST(noDuplicates, 0, noDuplicates.length - 1));
+    }
+
+    sortedArrayToBST(array, start, end) {
+        /* Base Case */
+        if (start > end)
+        {
+            return null;
+        }
+
+        /* Get the middle element and make it root */
+        var mid = Math.floor((start + end) / 2);
+        var node = new Node(array[mid]);
+
+        /* Recursively construct the left subtree and make it
+         left child of root */
+        node.left = this.sortedArrayToBST(array, start, mid - 1);
+
+        /* Recursively construct the right subtree and make it
+         right child of root */
+        node.right = this.sortedArrayToBST(array, mid + 1, end);
+
+        return node;
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    }
 }
